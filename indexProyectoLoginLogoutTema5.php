a9b174b268ffdd13322c93fb8797235b41bbbe07
SoniaALLSauces <sonia.antlla@educa.jcyl.es>
2021-12-03 13:03:51 +0100

    day 03/12/2021 - v.2

---
+++ indexProyectoLoginLogoutTema5.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<!-- Author: Sonia Antón Llanes -->
+<!-- Created on: 29-noviembre-2021 -->
+<!-- Index Proyecto Login Logout Tema 5 -->
+
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Sonia Anton Llanes - index Login Logout Tema 5</title>
+        <meta name="author" content="Sonia Antón Llanes">
+        <meta name="description" content="Proyecto DAW2">
+        <meta name="keywords" content="">
+        <link href="webroot/css/newcss.css" rel="stylesheet" type="text/css">
+        <link href="webroot/images/mariposa_vintage.png" rel="icon" type="image/png">	
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+        <style>
+            .button{text-align: center;}
+            .button>*{width: 150px;
+                      height: 80px;
+                      margin: 50px;
+                      padding: 20px;
+                      border: 2px solid #BF2411;
+                      border-radius: 50% 20%;
+                      background: #ecaaa1;
+                      font-size: 1.5rem;
+                      font-family: 'Secular One', sans-serif;
+                      font-weight: bolder;}
+        </style>
+    </head>
+    <body class="container">
+	<header class="header">
+            <h1 class="h1"><a href="../index.html" style="color: white">Desarrollo de Aplicaciones Web</a></h1>
+	</header>
+	<main class="main">
+            <section>
+                <h2><a href="../proyectoDWES/indexProyectoDWES.php">DWES</a> - Tema 5. Desarrollo de Aplicaciones Web utilizando Código Embebido</h2>
+                <h3>Proyecto Login Logout Tema 5 - 03/12/2021</h3>            
+                
+            </section>
+            <section class="button">
+                <button><a href="codigoPHP/login.php">LOG IN</a></button>
+            </section>
+        </main>
+        <footer class="footer">
+            <nav class="fnav">
+                <ul>
+                    <li class="ftexto"><a href="indexProyectoDWES.php">&copy 2020-21. Sonia Anton LLanes</a></li>
+                    <li>
+                        
+                        <a class="maxMedia" href="doc/curriculum_SALL.pdf" target="_blank"><img src="webroot/images/CV.png" alt="imagen_CV"></a>
+                        <a class="maxMedia" href=""><img src="webroot/images/linkedin.png" alt="imagen_linkedIn"></a>
+                        <a class="maxMedia" href="https://github.com/SoniaALLSauces" target="_blank"><img src="webroot/images/github.png" alt="imagen_github"></a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>       
+    </body>
+</html>
